e7d965f1b1a78dfd28288af50afac4d71de134c5
park-d <54589582+park-d@users.noreply.github.com>
2022-09-30 11:55:09 -0500

    just changed some comments

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-// Import our modular routers for /tips and /feedback
+// Import our modular routers for /api and /
 const apiRouter = require('./apiRoutes');
 const htmlRouter = require('./htmlRoutes');
 
