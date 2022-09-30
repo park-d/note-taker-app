b59bd7c7756ede7f75194ad499bd6c59bd2755db
park-d <54589582+park-d@users.noreply.github.com>
2022-09-30 11:33:53 -0500

    added routes for HTML files

--- server.js
+++ server.js
@@ -5,11 +5,25 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.json());
 app.use(express.static('public'));
+
 
 // GET Route for homepage
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+
+//route to notes.html
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+
+// GET Route for *
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
