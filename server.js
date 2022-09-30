60cf8327253a48b476ac562c7c1167337c43ee2a
park-d <54589582+park-d@users.noreply.github.com>
2022-09-30 11:39:42 -0500

    added get route for the saved notes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 
+const filePath = path.join(__dirname, "db/db.json")
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -19,6 +21,10 @@ app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
+// route to read the `db.json` file and return all saved notes as JSON.
+app.get("/api/notes", (req, res) => {
+    res.sendFile(filePath);
+});
 
 // GET Route for *
 app.get('*', (req, res) =>
